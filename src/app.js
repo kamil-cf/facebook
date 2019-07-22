6bd96d5eefb36d4071a66d214e0c098246a0931a
Kamil Zagrabski <kamil@couponfollow.com>
2019-07-22 10:39:56 +0200

    First endpoint

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "ok"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.`);
+});
