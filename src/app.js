9fad0293647029a3798382d74cae68857616a53b
Kamil Zagrabski <kamil@couponfollow.com>
2019-07-24 14:35:15 +0200

    Start with one instance

--- src/app.js
+++ src/app.js
@@ -1,25 +1,13 @@
 require = require("./perf")(require);
 
-const cluster = require("cluster");
-const os = require("os");
-// const {setup: memorySetup} = require("./memory");
-// memorySetup();
-
 const express = require("express");
 const app = express();
 
-if (cluster.isMaster) {
-    const cpus = os.cpus().length;
-    Array.from({length: cpus}).forEach(() => {
-        cluster.fork();
-    });
-} else {
-    require("./loaders")(app);
+require("./loaders")(app);
 
-    const port = parseInt(process.env.PORT);
-    const host = process.env.HOST;
+const port = parseInt(process.env.PORT);
+const host = process.env.HOST;
 
-    app.listen(port, host, () => {
-        console.log(`Server started on ${host}:${port}.`);
-    });
-}
+app.listen(port, host, () => {
+    console.log(`Server started on ${host}:${port}.`);
+});
