e0bd00119b3bda1495b4fdf77e4c300e34116697
Kamil Zagrabski <kamil@couponfollow.com>
2019-07-24 10:24:58 +0200

    Log performance metrics

--- src/perf.js
+++ src/perf.js
@@ -1,11 +1,14 @@
 const {performance, PerformanceObserver} = require("perf_hooks");
+const logger = require("../src/services/logger");
 
 module.exports = (fn) => {
     const timerified = performance.timerify(fn);
 
     const perf = new PerformanceObserver((list) => {
-        list.getEntries().forEach((entry) => {
+        const entries = list.getEntries();
+        entries.forEach((entry) => {
             console.log(entry.name, entry.duration);
+            logger.info(entries);
         });
     });
 
